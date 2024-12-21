b5d100cf7cb57cbf3dbc2b69ee6572e9c2f01efb
Jason Sprinkle <jason.sprinkle91@gmail.com>
2024-12-21 14:48:15 -0600

    actually get feed item data

--- src/pages/show.ts
+++ src/pages/show.ts
@@ -1,12 +1,30 @@
 import { html, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
+import { FeedItemUgh, fetchFeedItems } from "../actions/feed";
+import { Task } from "@lit/task";
 
 @customElement("sp-show")
 export class SpShow extends LitElement {
   @property({ type: String })
   guid: string;
 
-  render() {
+  private _showTask = new Task(this, {
+    task: async () => {
+      const feedItems = await fetchFeedItems();
+      return feedItems.find(({ guid }) => guid === this.guid);
+    },
+    args: () => [this.guid],
+  });
+
+  private _renderShow = (feedItemUgh: FeedItemUgh | undefined) => {
+    console.log(feedItemUgh);
     return html`<p>${this.guid}</p>`;
+  };
+
+  render() {
+    return this._showTask.render({
+      pending: () => html`<sp-loading-page></sp-loading-page>`,
+      complete: this._renderShow,
+    });
   }
 }
