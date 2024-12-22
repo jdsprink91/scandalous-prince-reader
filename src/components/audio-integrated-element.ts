5b5ecdb64a6155e661175acf4d43bdebd39f5a19
Jason Sprinkle <jason.sprinkle91@gmail.com>
2024-12-22 15:22:13 -0600

    more cleanup

--- src/components/audio-integrated-element.ts
+++ src/components/audio-integrated-element.ts
@@ -1,13 +1,37 @@
 import { html, LitElement } from "lit";
 import { state } from "lit/decorators.js";
 import { getAudioPlayer } from "../actions/audio";
-import { niceTime } from "../utils/date";
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 import duration from "dayjs/plugin/duration";
 
 dayjs.extend(customParseFormat);
 dayjs.extend(duration);
+
+function niceTime(duration: duration.Duration) {
+  const hours = duration.get("h");
+  const minutes = duration.get("m");
+  const hourString = hours
+    ? `${duration.get("h")} hour${hours > 1 ? "s" : ""}`
+    : "";
+  const minuteString = minutes
+    ? `${duration.get("m")} minute${minutes > 1 ? "s" : ""} `
+    : "";
+
+  if (hourString && !minuteString) {
+    return hourString;
+  }
+
+  if (hourString && minuteString) {
+    return `${hourString}, ${minuteString}`;
+  }
+
+  if (minuteString) {
+    return minuteString;
+  }
+
+  return null;
+}
 
 export class AudioIntegratedElement extends LitElement {
   @state()
