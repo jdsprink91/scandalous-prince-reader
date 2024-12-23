d6efecdbbdffa2cb971d7d1beb1d43f9767ceb22
Jason Sprinkle <jason.sprinkle91@gmail.com>
2024-12-22 18:04:10 -0600

    do something here

---
+++ src/pages/shows-with-feed.ts
@@ -0,0 +1,101 @@
+import { css, html, LitElement } from "lit";
+import { customElement } from "lit/decorators.js";
+import { getSPDB } from "../actions/database";
+import { Task } from "@lit/task";
+import { FeedTableRow } from "../types/database";
+import "../components/sp-show-img";
+import { deleteFeedFromCache } from "../actions/feed";
+
+@customElement("sp-shows-with-feed-page")
+export class SpShowWithFeedPage extends LitElement {
+  static styles = css`
+    .title-action-container {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    ul {
+      padding-left: 0;
+    }
+
+    li {
+      list-style: none;
+      display: flex;
+      padding-bottom: 0.5rem;
+    }
+
+    li + li {
+      padding-top: 0.5rem;
+      border-top: 1px solid black;
+    }
+
+    h2 {
+      margin: 0;
+    }
+
+    .show-title-info-container {
+      margin-left: 1rem;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      flex-grow: 1;
+    }
+
+    .actions-container {
+      display: flex;
+    }
+  `;
+
+  private _showsTask = new Task(this, {
+    task: async () => {
+      const db = await getSPDB();
+      return db.getAll("feed");
+    },
+    args: () => [],
+  });
+
+  private _deleteShow = async (show: FeedTableRow) => {
+    const db = await getSPDB();
+    const tx = db.transaction(["feed"], "readwrite");
+
+    // delete feed
+    const feedObjectStore = tx.objectStore("feed");
+    feedObjectStore.delete(show.link!);
+
+    // remove feed items from cache
+    deleteFeedFromCache(show.link);
+
+    // tell everyone that we're done
+    await tx.done;
+  };
+
+  private _renderShows = (feeds: FeedTableRow[]) => {
+    return html`
+      <div class="title-action-container">
+        <h1>Your Shows</h1>
+        <a href="/shows/add">Add a show</a>
+      </div>
+      <ul>
+        ${feeds.map((show) => {
+          return html`<li>
+            <sp-show-img .src=${show.image?.url}></sp-show-img>
+            <div class="show-title-info-container">
+              <h2>${show.title}</h2>
+              <div class="actions-container">
+                <button @click=${() => this._deleteShow(show)}>Delete</button>
+              </div>
+            </div>
+          </li>`;
+        })}
+      </ul>
+    `;
+  };
+
+  render() {
+    return this._showsTask.render({
+      pending: () => html`<sp-loading-page></sp-loading-page>`,
+      complete: this._renderShows,
+    });
+  }
+}
